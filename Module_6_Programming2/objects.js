7693ee9119ad39bf79ef07fcd19f58172d91a374
Janet <perezjanet103@gmail.com>
2020-08-13 10:23:56 -0400

    merging Module 5 with Master branch

---
+++ Module_6_Programming2/objects.js
@@ -0,0 +1,21 @@
+let car={
+    vehicleType: "Convertible",
+    name: "BMW",
+    color: "blue",
+    numberOfSeats: 4,
+
+    get vehicleType(){
+        return this.vehicleType;
+    },
+
+    set vehicleType(value){
+        this.color = value.toUpperCase();
+    }
+}
+
+car.vehicleType = "sports car";
+console.log(car.vehicleType);
+
+let team=
+
+let Animal=
